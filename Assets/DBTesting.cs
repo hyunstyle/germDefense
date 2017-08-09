d5b2f3eec56fb06ee6bb8e8ff71ab4a3778fd23a
Lee JongWon <jay09144@gmail.com>
2017-08-09 20:54:33 +0900

    Connect sqlite DB

---
+++ Assets/DBTesting.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class DBTesting : MonoBehaviour {
+
+    public DBfunction db;
+
+    public string message = "";
+
+	// Use this for initialization
+	void Start () {
+
+        db.CopyDB();
+        message += db.GetConStr() + "\n";
+        Debug.Log(message);
+        message += db.TestConnection() + "\n";
+        Debug.Log(message);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
